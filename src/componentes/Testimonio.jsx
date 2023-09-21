75fb9104033d8e7dd687b96ad08db79ce0e88239
David Espinosa <despinosa2550@gmail.com>
2023-09-21 14:02:40 -0600

    testimonials as an object on an apart file personas.js

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -7,12 +7,12 @@ function Testimonio(props) {
     <div className='contenedor-testimonio'>
       <img 
       className='imagen-testimonio' 
-      src={require(`../imagenes/${props.imagen}.png`)}
+      src={require(`../imagenes/${props.persona.imagen}.png`)}
       alt={imgAlt}/>
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
-        <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
-        <p className="texto-testimonio">"{props.testimonio}"</p>
+        <p className="nombre-testimonio"><strong>{props.persona.nombre}</strong> en {props.persona.pais}</p>
+        <p className="cargo-testimonio">{props.persona.cargo} en <strong>{props.persona.empresa}</strong></p>
+        <p className="texto-testimonio">"{props.persona.testimonio}"</p>
       </div>
     </div>
   );
